1119c42ea5bd8553b18fdb2b544b9dd6a776953c
Kyle Rogers <kylerogers334@gmail.com>
2017-07-24 22:52:37 +0000

    add config for transpiling, colors, and sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './client/js/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist'
     }
 };
