0cbc9c5e8f9ed3df9c6c51cc33da63131b3936c5
Kyle Rogers <kylerogers334@gmail.com>
2017-07-26 20:22:57 +0000

    App now logs state name on click

--- client/components/Map.js
+++ client/components/Map.js
@@ -14,18 +14,26 @@ export default class Map extends React.Component {
     
     createMap() {
         const svg = d3.select('svg');
-        const path = d3.geoPath(); 
-        
+        const path = d3.geoPath();
+
         // needs to pull from either db, or just serve within html
         d3.json('../../data/us-10m.v1.json', function(error, us) {
             if (error) throw error;
-
+            
+            const data = topojson.feature(us, us.objects.states).features;
+            
             svg.append('g')
                 .attr('class', 'states')
                 .selectAll('path')
-                .data(topojson.feature(us, us.objects.states).features)
+                .data(data)
                 .enter().append('path')
-                .attr('d', path);
+                .attr('d', path)
+                .attr('id', function(d, i) {
+                    return us.objects.states.geometries[i].info.name;
+                })
+                .on('click', function(d, i) {
+                    console.log(d3.select(this).attr('id'));
+                });
             
             svg.append('path')
                 .attr('class', 'state-borders')
@@ -36,7 +44,7 @@ export default class Map extends React.Component {
     render() {
         return (
             <div id='map'>
-                <svg width="960" height="600"></svg>
+                <svg width='960' height='600'></svg>
             </div>
         );
     }
