0296846b1a2f651a211762a88ff457f41c2abb75
Kyle Rogers <kylerogers334@gmail.com>
2017-07-30 18:42:56 +0000

    scaling formula perfected

--- client/components/EnlargedState.js
+++ client/components/EnlargedState.js
@@ -9,9 +9,17 @@ export class EnlargedState extends React.Component {
     }
     
     componentDidMount() {
-        d3.select('.state-enlarged').on('click', function(d, i) {
-            console.log('state clicked');
-        });
+        const state = d3.select('.state-enlarged');
+
+        const dimensions = state.node().getBBox();
+        const scale = (dimensions.x > dimensions.y) ?
+            Math.floor(600 / dimensions.height) :
+            Math.floor(960 / dimensions.width);
+        // console.log(dimensions);
+        // console.log('scale', scale)
+        const tX = (-scale) * dimensions.x + 50;
+        const tY = (-scale) * dimensions.y + 50;
+        state.attr('transform', `translate(${tX}, ${tY}) scale(${scale}, ${scale})`);
     }
     
     render() {
@@ -19,6 +27,7 @@ export class EnlargedState extends React.Component {
             <path
                 d={this.props.enlargedState.attributes[0].value}
                 className='state-enlarged'
+                id={this.props.enlargedState.id}
             ></path>
         );
     }
