01ff3101e1ef2d8c04c6995b10e05d1099140c31
Kyle Rogers <kylerogers334@gmail.com>
2017-08-01 23:55:43 +0000

    remove old console.logs

--- client/helpers/stateHelpers.js
+++ client/helpers/stateHelpers.js
@@ -9,14 +9,12 @@ export const stateHelper = category => {
 
 function stateUnemploymentHelper() {
     const reactThis = this;
-    console.log('reactThis ->', reactThis);
     const color = d3.scaleThreshold()
                 .domain(d3.range(2, 16))
                 .range(d3Chromatic.schemeBlues[9]);
     
     d3.select('#states-container').selectAll('path')
         .style('fill', function(d, i) {
-            console.log('d3 this -> ', this);
             const state = d3.select(this).attr('id');
             const match = reactThis.props.categoryData
                         .find(function(d) {if (d.name === state) return d;});
