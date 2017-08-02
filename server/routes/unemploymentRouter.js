3ecccea1d92fb3662d98791246add6c6c5375402
Kyle Rogers <kylerogers334@gmail.com>
2017-08-02 03:03:02 +0000

    county unemployment data endpoint active

--- server/routes/unemploymentRouter.js
+++ server/routes/unemploymentRouter.js
@@ -3,11 +3,22 @@ const router = express.Router();
 
 const knex = require('../../config.js').knex;
 
-router.get('/', function(req, res) {
-    // respond with state unemployment
+router.get('/state', function(req, res) {
     knex
         .select('*')
         .from('unemployment_state')
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+router.get('/county', function(req, res) {
+    knex
+        .select('*')
+        .from('unemployment_county')
         .then(results => {
             res.json(results);
         })
