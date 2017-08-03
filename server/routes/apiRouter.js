f2c5ac70a8280564df5d2101aab3911debf25986
Kyle Rogers <kylerogers334@gmail.com>
2017-08-03 21:31:29 +0000

    add route to server state population

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+
 const unemploymentRouter = require('./unemploymentRouter');
+const populationRouter = require('./populationRouter');
+router.use('/population', populationRouter);
 router.use('/unemployment', unemploymentRouter);
 
 router.get('/', function(req, res) {
---
+++ server/routes/populationRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const knex = require('../../config.js').knex;
+
+router.get('/state', function(req, res) {
+    knex
+        .select('*')
+        .from('population_state')
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+router.get('/county/:fipsID', function(req, res) {
+    knex
+        .select('*')
+        .from('population_county')
+        .where(knex.raw(`SUBSTRING(fips, 1, 2)='${req.params.fipsID}'`))
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+module.exports = router;
