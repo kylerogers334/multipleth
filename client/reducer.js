f48c4bbc0ea0b4f77bf92614fb77f56826c04368
Kyle Rogers <kyle.rogers@benefitfocus.com>
2019-06-12 19:40:50 -0400

    linting clean up

--- client/reducer.js
+++ client/reducer.js
@@ -14,60 +14,63 @@ const initialState = {
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case types.LOAD_US_STATES_DATA:
-			return Object.assign({}, state, {
-				usStatesLineData: action.usStatesLineData
-			});
+			return { ...state, usStatesLineData: action.usStatesLineData };
 
 		case types.CHANGE_COLOR:
-			return Object.assign({}, state, { color: action.color });
+			return { ...state, color: action.color };
 
 		case types.SHOW_OVERLAY:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				displayOverlay: true,
 				enlargedState: action.enlargedState
-			});
+			};
 
 		case types.HIDE_OVERLAY:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				displayOverlay: false,
 				enlargedState: null
-			});
+			};
 
 		case types.CLEAR_MAP:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				categoryStateData: null,
 				categoryCountyData: null,
 				categoryName: null
-			});
+			};
 
 		case types.FETCH_CATEGORY_STATE_SUCCESS:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				categoryStateData: action.data,
 				categoryName: action.category
-			});
+			};
 
 		case types.FETCH_CATEGORY_STATE_ERROR:
 			console.error(action.error);
 			return state;
 
 		case types.FETCH_CATEGORY_COUNTY_SUCCESS:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				categoryCountyData: action.data,
 				categoryName: action.category
-			});
+			};
 
 		case types.FETCH_CATEGORY_COUNTY_ERROR:
 			console.error(action.error);
 			return state;
 
 		case types.SHOW_ABOUT_MODAL:
-			return Object.assign({}, state, { showInfoModal: 'about' });
+			return { ...state, showInfoModal: 'about' };
 
 		case types.SHOW_HELP_MODAL:
-			return Object.assign({}, state, { showInfoModal: 'help' });
+			return { ...state, showInfoModal: 'help' };
 
 		case types.HIDE_MODAL:
-			return Object.assign({}, state, { showInfoModal: false });
+			return { ...state, showInfoModal: false };
 
 		default:
 			return state;
